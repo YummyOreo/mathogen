efc040d92cc3d76bd359b2856bcbad0686421c15
YummyOreo <bobgim20@gmail.com>
2022-10-26 17:22:57 -0500

    best commit message

--- explore.py
+++ explore.py
@@ -90,7 +90,7 @@ def circle(context: cairo.Context, x: float, y: float, width: float, height: flo
     context.stroke()
     context.restore()
 
-def arch1(context: cairo.Context, x: List[float], y: List[float], color: List[float], invert: bool, multi_x: float, multi_y: float):
+def curve1(context: cairo.Context, x: List[float], y: List[float], color: List[float], invert: bool, multi_x: float, multi_y: float):
     context.set_line_width(0.01)
     set_rgba(context, color)
 
@@ -139,18 +139,86 @@ def mathTex(context: cairo.Context, x: float, y: float, width: float, height: fl
     # returns this because it will be different than the one inputted as it is dependent on the image created by matplotlib
     return [width_ratio, height_ratio]
 
-def arch2(context: cairo.Context, pos_start: List[float]):
-    print(pos_start)
-    context.save()
-    context.restore()
-    pass
+def curve2(context: cairo.Context, pos_start: List[float]):
+    context.set_line_width(0.01)
 
+    x, y = pos_start
+    x1, y1 = 0.4, 0.9
+    x2, y2 = 0.6, 0.1
+    x3, y3 = 0.9, 0.5
+
+    context.move_to(x, y)
+    context.curve_to(x1, y1, x2, y2, x3, y3)
+
+    context.stroke()
+
+    circle(context, x, y, .05, .05, [0, 0, 0, 1], 2)
+
+    circle(context, x1, y1, .05, .05, [0, 0, 0, 1], 2)
+
+    circle(context, x2, y2, .05, .05, [0, 0, 0, 1], 2)
+
+    circle(context, x3, y3, .05, .05, [0, 0, 0, 1], 2)
+
+    # context.set_source_rgba(1, 0.2, 0.2, 0.6)
+    # context.set_line_width(0.03)
+    # context.move_to(x, y)
+    # context.line_to(x1, y1)
+    # context.move_to(x2, y2)
+    # context.line_to(x3, y3)
+    # context.stroke()
+
+def arc1(context: cairo.Context, x: float, y: float):
+    context.set_line_width(0.01)
+
+    # The larger the radius, the larger the arch will be
+    radius = 0.1
+
+    angle1 = 45.0 * (math.pi / 180.0)  # angles are specified
+    angle2 = 180.0 * (math.pi / 180.0)  # in radians
+
+    context.arc(x, y, radius, angle1, angle2,)
+    context.stroke()
+
+
+    # draws lines based on the arc/angle
+    context.set_source_rgba(1, 0.2, 0.2, 0.6)
+    context.set_line_width(0.01)
+
+    # defindes te arch for the line to start and end at
+    context.arc(x, y, radius + 0.2, angle1, angle1)
+    context.line_to(x, y)
+
+
+    context.stroke()
+
+    context.arc(x, y, radius + 0.2, angle2, angle2)
+    context.line_to(x, y)
+    context.stroke()
+
+def arcTo(context: cairo.Context, x, y, slope_1, slope_2):
+    # Get slopes
+    # Get the angle between the 2 lines
+    # Get the starting angle and ending angle needed for the arc
+    # Render the arc
+    arc1(context, x, y)
+
+def line(context: cairo.Context, x: float, y: float, end_x: float, end_y: float):
+    set_rgba(context, [0, 0, 0, 1])
+    context.set_line_width(0.01)
+    context.move_to(x, y)
+    context.line_to(end_x, end_y)
+    context.stroke()
+
+    # The slope of the line
+    return (end_y - y) / (end_x - x)
 """
 Add rotation to everything!
 Everything is drawn on top of each other (if 2 things will take the same space, the last thing will be on top of the other)
 """
 with cairo.SVGSurface("example.svg", 1000, 1000) as surface:
     context = cairo.Context(surface)
+
     # Can't change this
     # To change the scale, do context.set_font_size(x)
     # what everything will be * by (1 = scaled num). have inputs be / by this num
@@ -177,9 +245,21 @@ with cairo.SVGSurface("example.svg", 1000, 1000) as surface:
     for a curve: you can choose from a x offset and a y offset
     you can also choose from a inverted or not
     """
-    arch1(context, [0.1, 0.1], [0.1, 0.7],[82, 182, 209, 1], False, 0.2, 0)
+    # curve1(context, [0.1, 0.1], [0.1, 0.7],[82, 182, 209, 1], False, 0.2, 0)
     """
     Trying to improve arch1
     """
-    arch2(context, [0.1, 0.1])
+    # curve2(context, [0.1, 0.5])
 
+    arc1(context, 0.5, 0.1)
+
+    """
+    Gets a point on a line
+    You should convert this into a function
+    then make a func to join 2 points
+    """
+    slope = line(context, 0.1, 0.2, 0.2, 0.3)
+    point_x = 0.2
+    point_y = (slope * point_x) + 0.1
+    circle(context, point_x, point_y, 0.03, 0.03, [82, 182, 209, 1], 2)
+    arcTo(context, 0.1, 0.2, point_x, point_x)
